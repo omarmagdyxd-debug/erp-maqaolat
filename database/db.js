23d875c2033c5a565dbc436b536e9dd3c8513ddb
omarmagdyxd-debug <omarmagdy.xd@gmail.com>
2026-06-16 13:49:08 +0300

    تحديث الاتصال بقاعدة بيانات Aiven

--- database/db.js
+++ database/db.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool.promise();
